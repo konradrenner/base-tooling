8856ef45b7af5c8e3a40582dfb587074c3a918d0
Konrad Renner <konrad.renner@fullstacks.eu>
2026-02-26 08:50:04 +0100

    darwing prompt

--- home/darwin.nix
+++ home/darwin.nix
@@ -12,19 +12,16 @@
     dotDir = config.home.homeDirectory;
 
     initContent = ''
-      # Enable vcs_info
       autoload -Uz vcs_info
       precmd() { vcs_info }
 
-      # Configure git format
       zstyle ':vcs_info:git:*' formats ' (%b%u%c)'
       zstyle ':vcs_info:git:*' actionformats ' (%b|%a%u%c)'
       zstyle ':vcs_info:git:*' stagedstr '+'
       zstyle ':vcs_info:git:*' unstagedstr '*'
 
-      # Prompt
       setopt PROMPT_SUBST
-      PROMPT='%F{green}%n@%m%f:%F{blue}%~%f%F{yellow}${vcs_info_msg_0_}%f%(!.#.$) '
+      PROMPT='%F{green}%n@%m%f:%F{blue}%~%f%F{yellow}$vcs_info_msg_0_%f%(!.#.$) '
     '';
   };
 
