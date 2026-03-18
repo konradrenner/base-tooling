95d762620c989ca9a61265ee950cd8d6b9bcd652
Konrad Renner <konrad.renner@fullstacks.eu>
2026-03-18 10:47:32 +0100

    cleanups

--- home/common.nix
+++ home/common.nix
@@ -28,6 +28,7 @@ in
 
   programs.zsh = {
     enable = true;
+    dotDir = "${config.xdg.configHome}/zsh";
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
